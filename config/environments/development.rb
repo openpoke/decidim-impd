a8c57e4d0a20b09d768f943c39e56d3444b924a8
David Igón <david@pokecode.net>
2025-11-03 17:31:55 +0100

    rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -64,7 +66,6 @@ Rails.application.configure do
   config.active_job.verbose_enqueue_logs = true
 
   # Suppress logger output for asset requests.
-  
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
