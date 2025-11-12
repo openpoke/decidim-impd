2845feb5250c4ca3690929df1517cba184bf6392
David Igón <david@pokecode.net>
2025-11-12 15:36:59 +0100

    add cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.asset_host = ENV["RAILS_ASSET_HOST"] if ENV["RAILS_ASSET_HOST"].present?
 
+  config.cors_enabled = Decidim::Env.new("DECIDIM_CORS_ENABLED").to_boolean_string
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
